a61ee63b736adbf346a5f6534d3f20c7a86fb2fb
Oleg <evolrn89@gmail.com>
2024-11-07 16:51:02 +0600

    Created component Calculator for mathematical calculations. The component uses calculatorSlice.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import KeypadAccess from './components/KeypadAccess/KeypadAccess.tsx';
 import NavBar from './components/NavBar/NavBar.tsx';
+import Calculator from './components/Calculator/Calculator.tsx';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
     <Routes>
       <Route path="/" element={<KeypadAccess/>} />
       <Route path="/access-keypad" element={<KeypadAccess/>} />
+      <Route path="/calculator" element={<Calculator/>} />
       <Route path="*" element={<h1>Page not found</h1>} />
     </Routes>
     </>
---
+++ src/components/Calculator/Calculator.tsx
@@ -0,0 +1,152 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../../app/store.ts';
+import { Box, Button, Paper, Typography } from '@mui/material';
+import { addInput, removeInput, resetInput, submitInput } from '../../slices/calculatorSlice.ts';
+
+const keys = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.'];
+const operations = ['+', '-', '*', '/'];
+
+const Calculator = () => {
+  const dispatch = useDispatch();
+  const { input, result } = useSelector((state: RootState) => state.calculator);
+
+  const handleClick = (value: string) => {
+    dispatch(addInput(value));
+  };
+
+  const handleDelete = () => {
+    dispatch(removeInput());
+  };
+
+  const handleReset = () => {
+    dispatch(resetInput());
+  };
+
+  const handleSubmit = () => {
+    dispatch(submitInput());
+  };
+
+  return (
+    <Box display="flex" justifyContent="center" alignItems="center" flexDirection="column">
+      <Typography variant="h2" marginBottom="40px">
+        Calculator
+      </Typography>
+      <Paper
+        style={{
+          padding: 20,
+          textAlign: 'center',
+          width: '350px',
+          height: '480px',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'space-between',
+          backgroundColor: '#f0f0f0',
+        }}
+      >
+        <Box
+          style={{
+            height: '120px',
+            borderRadius: '8px',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            fontSize: '32px',
+            overflowY: 'auto',
+            width: '100%',
+            backgroundColor: '#fff'
+          }}
+        >
+          <Typography
+            variant="h6"
+            style={{
+              fontSize: '32px',
+              fontWeight: 400,
+              color: result === 'Error' ? 'red' : 'black',
+              wordWrap: 'break-word',
+              maxWidth: '90%',
+              textAlign: 'center',
+            }}
+          >
+            {result !== null ? result : input}
+          </Typography>
+        </Box>
+
+
+        <Box display="flex" justifyContent="space-between" width="100%" marginTop="20px">
+          <Box
+            display="grid"
+            gridTemplateColumns="repeat(3, 1fr)"
+            gap={'4px'}
+            width="70%"
+          >
+            {keys.map((key) => (
+              <Button
+                key={key}
+                variant="contained"
+                onClick={() => handleClick(key)}
+                style={{
+                  height: '50px'
+                }}
+              >
+                {key}
+              </Button>
+            ))}
+            <Button
+              variant="contained"
+              onClick={handleDelete}
+              color="primary"
+              fullWidth
+              style={{ marginRight: '10px', height: '50px'}}
+            >
+              {'<'}
+            </Button>
+          </Box>
+
+          <Box
+            display="grid"
+            gridTemplateRows="repeat(4, 1fr)"
+            gap={'4px'}
+            width="25%"
+          >
+            {operations.map((operation) => (
+              <Button
+                key={operation}
+                variant="contained"
+                onClick={() => handleClick(operation)}
+              >
+                {operation}
+              </Button>
+            ))}
+          </Box>
+        </Box>
+
+        <Box
+          display="flex"
+          justifyContent="space-between"
+        >
+
+          <Button
+            variant="contained"
+            onClick={handleReset}
+            color="secondary"
+            fullWidth
+            style={{ marginRight: '10px', height: '50px', }}
+          >
+            C
+          </Button>
+
+          <Button
+            variant="contained"
+            onClick={handleSubmit}
+            color="secondary"
+            fullWidth
+          >
+            =
+          </Button>
+        </Box>
+      </Paper>
+    </Box>
+  );
+};
+
+export default Calculator;
